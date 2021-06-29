2c668151f268eb3b28724111bbeb5db3a6ce02ea
CyrusWarner <cyruswarner27@gmail.com>
2021-06-29 10:21:09 -0500

    Created a display for list of superheroes and also added a Delete and edit button.

--- superheroes/superheroesapp/urls.py
+++ superheroes/superheroesapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 app_name = 'superheroesapp'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # path('<int:superhero_id>/', views.superhero_detail, name='detail'),
+    path('new/', views.create_new_superhero, name='create_new_superhero')
 ]
--- superheroes/superheroesapp/views.py
+++ superheroes/superheroesapp/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'superheroesapp/index.html')
+
+    all_superheroes = Superhero.objects.all()
+    context = {
+        'all_superheroes': all_superheroes
+    }
+
+    return render(request, 'superheroesapp/index.html', context)
+
+
+
+def superhero_detail(request):
+    pass
 
 
 def create_new_superhero(request):
-    pass
+    if request.method == 'POST':
+        superhero_name = request.POST.get('name')
+        superhero_alter_ego = request.POST.get('alter_ego')
+        superhero_primary_ability = request.POST.get('primary_superhero_ability')
+        superhero_secondary_ability = request.POST.get('secondary_superhero_ability')
+        superhero_catchphrase = request.POST.get('superhero_catchphrase')
+        new_superhero = Superhero(superhero_name=superhero_name, alter_ego_name=superhero_alter_ego, primary_hero_ability=superhero_primary_ability, secondary_hero_ability=superhero_secondary_ability, superhero_catchphrase=superhero_catchphrase)
+        new_superhero.save()
+
+        return HttpResponseRedirect(reverse('superheroesapp:index'))
+    else:
+        return render(request, 'superheroesapp/index.html')
 
 
 def edit(request):
     pass
 
 
-def delete(request):
+def delete_superhero(request):
     pass
